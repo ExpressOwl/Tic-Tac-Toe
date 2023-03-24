448c7b3efde5d8b832eef6dd2da37a1a467494e5
Sean <110854028+ExpressOwl@users.noreply.github.com>
2023-03-24 00:07:53 +0000

    Replaced styling with a root
    The root colours use a hue of 10 and will be wrapped in an HSL, using --acc, --bkg or --text depending on what is needed.
    
    Also, .join() is used at the end of the map for the making the tiles as otherwise the commas will be in the way.

--- tictactoe.js
+++ tictactoe.js
@@ -1,58 +1,21 @@
-let board;
-let playerO = 'O';
-let playerX = 'X';
-let currentPlayer = playerO;
-let gameOver = false;
+const gameBoard = document.querySelector("#board");
+const info = document.querySelector("#info");
+// Initialize turn system
+let turn;
 
-window.onload = function() {
-  setGame();
+// creating the game board
+function createGameBoard() {
+  // Create the empty 3 x 3 board
+  const emptyTiles = " ".repeat(9).split("");
+  // Give button functionality to each tile using map, could also use for if wanted
+  // .join is needed at the end to get rid of the commas, so it is basically one long string of HTML
+  const tileGrid = emptyTiles
+    .map((tile) => `<button class="tile"></button>`)
+    .join("");
+
+  gameBoard.innerHTML = tileGrid;
+  turn = "X";
+  info.textContent = `${turn}'s turn`;
 }
 
-// Function used to populate the board
-function setGame() {
-  board = [
-    [" ", " ", " "],
-    [" ", " ", " "],
-    [" ", " ", " "]
-  ]
-
-  for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-      // <div id="0-0"></div>
-      let tile = document.createElement("div");
-      tile.id = i.toString() + "-" + j.toString();
-      tile.classList.add("tile");
-      if (i === 0 || i === 1) {
-        tile.classList.add('horizontal-lines')
-      } 
-      if (j === 0 || j === 1) {
-        tile.classList.add('vertical-lines')
-      }
-      tile.addEventListener('click', setTile);
-      document.getElementById("board").append(tile)
-    }
-  }
-} 
-
-function setTile() {
-  if (gameOver) {
-    return;
-  }
-
-  let coordinates = this.id.split("-")
-  let i = parseInt(coordinates[0]);
-  let j = parseInt(coordinates[1]);
-
-  if (board[i][j] != ' ') {
-    return;
-  }
-
-  board[i][j] = currentPlayer;
-  this.innerText = currentPlayer;
-
-  if (currentPlayer === playerO) {
-    currentPlayer = playerX
-  } else {
-    currentPlayer = playerO;
-  }
-}
+createGameBoard();
